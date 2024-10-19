ee1201c0809ab9056adacffcf59433e1dcfe233e
icho-plus <slimkt623@gmail.com>
2024-10-20 00:31:26 +0900

    repush

---
+++ What_is_this_insect_applicationpp/libuvccamera/src/main/obj/local/armeabi-v7a/objs/UVCCamera/RotateImage.o.d
@@ -0,0 +1,21 @@
+./obj/local/armeabi-v7a/objs/UVCCamera/RotateImage.o: \
+  jni/UVCCamera/RotateImage.cpp jni/UVCCamera/RotateImage.h \
+  jni/UVCCamera/libUVCCamera.h \
+  jni/libusb/android/jni/../../libusb/libusb.h \
+  jni/libuvc/android/jni/../../include/libuvc/libuvc.h \
+  jni/libuvc/android/jni/../../include/libuvc/libuvc_config.h \
+  jni/UVCCamera/../utilbase.h jni/UVCCamera/../localdefines.h
+
+jni/UVCCamera/RotateImage.h:
+
+jni/UVCCamera/libUVCCamera.h:
+
+jni/libusb/android/jni/../../libusb/libusb.h:
+
+jni/libuvc/android/jni/../../include/libuvc/libuvc.h:
+
+jni/libuvc/android/jni/../../include/libuvc/libuvc_config.h:
+
+jni/UVCCamera/../utilbase.h:
+
+jni/UVCCamera/../localdefines.h:
